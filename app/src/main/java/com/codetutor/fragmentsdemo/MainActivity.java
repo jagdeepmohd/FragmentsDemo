60ff0d7a6549ffc7d75ebce2c3bb6a660fcdf016
CORP\jmohmmed <jagdeep.mohmmed@capgemini.com>
2018-02-07 18:58:56 +0530

    First commit of Fragment Demo

---
+++ app/src/main/java/com/codetutor/fragmentsdemo/MainActivity.java
@@ -0,0 +1,124 @@
+package com.codetutor.fragmentsdemo;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String COMMON_TAG = "CombinedLifeCycle";
+    private static final String ACTIVITY_NAME = MainActivity.class.getSimpleName();
+    private static final String TAG = ACTIVITY_NAME;
+
+    FragmentManager fragmentManager;
+    FragmentTransaction fragmentTransaction;
+
+    private Button buttonAddFragment;
+    private TextView textViewFragmentCount;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        buttonAddFragment = (Button)findViewById(R.id.buttonAddFragment);
+        textViewFragmentCount = (TextView)findViewById(R.id.textViewFragmentCount);
+
+        fragmentManager=getSupportFragmentManager();
+
+
+        textViewFragmentCount.setText("Fragment count in back stack: "+fragmentManager.getBackStackEntryCount());
+
+        fragmentManager.addOnBackStackChangedListener(new FragmentManager.OnBackStackChangedListener() {
+            @Override
+            public void onBackStackChanged() {
+                textViewFragmentCount.setText("Fragment count in back stack: "+fragmentManager.getBackStackEntryCount());
+            }
+        });
+
+        Log.i(TAG,"Initial BackStackEntryCount: "+fragmentManager.getBackStackEntryCount());
+
+
+        buttonAddFragment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                addFragment();
+            }
+        });
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    private void addFragment(){
+        Fragment fragment;
+
+        /*switch (fragmentManager.getBackStackEntryCount()){
+            case 0: fragment = new SampleFragment(); break;
+            case 1: fragment = new FragmentTwo();break;
+            case 2: fragment = new FragmentThree(); break;
+            default: fragment = new SampleFragment(); break;
+        }*/
+
+        fragment = fragmentManager.findFragmentById(R.id.fragmentContainer);
+        if(fragment instanceof SampleFragment){
+            fragment = new FragmentTwo();
+        }else if(fragment instanceof FragmentTwo){
+            fragment = new FragmentThree();
+        }else if(fragment instanceof FragmentThree){
+            fragment = new SampleFragment();
+        }else{
+            fragment = new SampleFragment();
+        }
+
+        fragmentTransaction=fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.fragmentContainer,fragment,"demofragment");
+ //       fragmentTransaction.addToBackStack(null);
+        fragmentTransaction.commit();
+    }
+
+    @Override
+    public void onBackPressed() {
+        fragmentTransaction = fragmentManager.beginTransaction();
+        Fragment fragment = fragmentManager.findFragmentById(R.id.fragmentContainer);
+        if(fragment!=null){
+            fragmentTransaction.remove(fragment);
+            fragmentTransaction.commit();
+        }else{
+            super.onBackPressed();
+        }
+
+    }
+}
